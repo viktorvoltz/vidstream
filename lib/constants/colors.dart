5bae9c7f0387ee920aed535749e847225d68bd47
viktorvoltz <vctrchinyeaka@gmail.com>
2022-04-11 13:10:21 +0100

    colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CustomColors{
+  static Color muxPink = const Color.fromARGB(255, 199, 6, 70);
+  static Color muxGray = const Color.fromARGB(255, 65, 63, 63);
+  static Color muxPinkLight =  const Color.fromARGB(255, 214, 90, 162);
+}
