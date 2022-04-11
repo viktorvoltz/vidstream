da76b600e91c688c2240fd12def9df179d338910
viktorvoltz <vctrchinyeaka@gmail.com>
2022-04-11 11:59:57 +0100

    asset data

---
+++ lib/models/asset_data.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:vidstream/models/data.dart';
+
+class AssetData {
+  AssetData({
+    this.data,
+  });
+
+  List<Data>? data;
+
+  factory AssetData.fromRawJson(String str) =>
+      AssetData.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory AssetData.fromJson(Map<String, dynamic> json) => AssetData(
+        data: json["data"] == null
+            ? null
+            : List<Data>.from(json["data"].map((x) => Data.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "data": data == null
+            ? null
+            : List<dynamic>.from(data!.map((x) => x.toJson())),
+      };
+}
