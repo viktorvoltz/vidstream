7bad73ecd1298c5e0a9b6067e6671ed44ca94a02
viktorvoltz <vctrchinyeaka@gmail.com>
2022-04-11 11:36:16 +0100

    init

---
+++ lib/constants/string.dart
@@ -0,0 +1,22 @@
+const muxBaseUrl = 'https://api.mux.com';
+
+// API server running on localhost
+const muxServerUrl = 'http://localhost:3000';
+
+// API for generating thumbnails of a video
+const muxImageBaseUrl = 'https://image.mux.com';
+
+// API for streaming a video
+const muxStreamBaseUrl = 'https://stream.mux.com';
+
+// Received video file format
+const videoExtension = 'm3u8';
+
+// Thumbnail file type and size
+const imageTypeSize = 'thumbnail.jpg?time=5&width=200';
+
+// Content Type used in API calls
+const contentType = 'application/json';
+
+// Test video url provided by MUX
+const demoVideoUrl = 'https://storage.googleapis.com/muxdemofiles/mux-video-intro.mp4';
